a6c626cf34cf6f484b40388619a69035b7ac848d
Sofia Ibragimova <ibragimova2310@yandex.ru>
2023-04-03 21:26:19 +0700

    в Сoverter.java (заменила double на int + упростила код)

--- src/Converter.java
+++ src/Converter.java
@@ -1,13 +1,12 @@
 public class Converter {
 
-    double oneStep = 0.75;
-    double calories = 50;
+
     void convertToKm(int allStep){
-        double kilometers =  (allStep*oneStep)/ 1000;
+        int kilometers = ((int) (allStep*0.75)/ 1000);
         System.out.println("Ты прошел: " + kilometers + " км");
     }
     void convertStepsToKilocalories(int allStep){
-        double kilocalories = (calories*allStep)/1000;
+        int kilocalories = (50*allStep)/1000;
         System.out.println("Ого, ты сжог: "+kilocalories+" Килокалорий");
     }
 }
